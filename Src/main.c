ae7b5c9690804ac07f97afc93710825acad7a445
erfan <erfanlessan1@gmail.com>
2026-09-12 11:55:11 +0100

    Testing making commits

--- Src/main.c
+++ Src/main.c
@@ -87,6 +87,7 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  // Here is a test comment
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
