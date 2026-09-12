68fa93e7205eb945c83729aa540908684d52ccb3
erfan <erfanlessan1@gmail.com>
2026-09-12 20:48:47 +0100

    Added changelog markdown file

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 2026-09-12
+- Cut out CubeMX generated comments for neatness. These will likely reappear
+in main.c once I generate a new .ioc file. 
--- Src/main.c
+++ Src/main.c
@@ -4,18 +4,9 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usart.h"
@@ -145,9 +136,6 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
